ad9f00e3b0a1305f996e881f695f536ef06f67d4
Kevin Zúñiga <kevin.zun@gmail.com>
2022-04-04 08:36:36 -0500

    fix: remove wrapping div from form

--- src/lib/Form.svelte
+++ src/lib/Form.svelte
@@ -6,27 +6,23 @@
     };
 </script>
 
-<div
-    class="fixed top-60 left-60 w-96 border border-gray-900 bg-blue-100/75 p-4"
->
-    <form on:submit|preventDefault={submit}>
-        <div>
-            <label for="title">Title</label>
-            <input name="title" class="border border-gray-900" id="title" />
-        </div>
+<form on:submit|preventDefault={submit}>
+    <div>
+        <label for="title">Title</label>
+        <input name="title" class="border border-gray-900" id="title" />
+    </div>
 
-        <div>
-            <label for="front">Front</label>
-            <textarea name="front" id="front" />
-        </div>
+    <div>
+        <label for="front">Front</label>
+        <textarea name="front" id="front" />
+    </div>
 
-        <div>
-            <label for="back">Back</label>
-            <textarea name="back" id="back" />
-        </div>
+    <div>
+        <label for="back">Back</label>
+        <textarea name="back" id="back" />
+    </div>
 
-        <div>
-            <button type="submit" class="bg-gray-300 p-1">Submit</button>
-        </div>
-    </form>
-</div>
+    <div>
+        <button type="submit" class="bg-gray-300 p-1">Submit</button>
+    </div>
+</form>
