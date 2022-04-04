97789c408bd96eda2837931feaa9e1e2ba22671b
Kevin Zúñiga <kevin.zun@gmail.com>
2022-04-04 08:35:56 -0500

    feat: add window component

---
+++ src/lib/Window.svelte
@@ -0,0 +1,83 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import { fromEvent } from 'rxjs';
+    import {
+        map,
+        startWith,
+        pairwise,
+        scan,
+        takeUntil,
+        mergeMap,
+    } from 'rxjs/operators';
+
+    export let x: number = 0;
+    export let y: number = 0;
+    export let w: number = 480;
+    export let h: number = 320;
+
+    const startPosition = { x, y };
+
+    let titleBar: HTMLElement;
+
+    onMount(() => {
+        const mouseDown$ = fromEvent(titleBar, 'mousedown');
+        const mouseUp$ = fromEvent(window.document, 'mouseup');
+        const mouseMove$ = fromEvent(window.document, 'mousemove');
+
+        const sub = mouseDown$
+            .pipe(
+                mergeMap((mouseDownEvent: MouseEvent) => {
+                    return mouseMove$.pipe(
+                        startWith(mouseDownEvent),
+                        pairwise(),
+                        map(([ev1, ev2]: [MouseEvent, MouseEvent]) => {
+                            return {
+                                dx: ev2.clientX - ev1.clientX,
+                                dy: ev2.clientY - ev1.clientY,
+                            };
+                        }),
+                        takeUntil(mouseUp$)
+                    );
+                }),
+                scan(
+                    (s, m) => ({
+                        x: s.x + m.dx,
+                        y: s.y + m.dy,
+                    }),
+                    startPosition
+                )
+            )
+            .subscribe((state) => {
+                x = state.x;
+                y = state.y;
+            });
+
+        return () => {
+            sub.unsubscribe();
+        };
+    });
+</script>
+
+<div
+    style="left: {x}px; top: {y}px; width: {w}px; height: {h}px;"
+    class="waves window rounded border border-gray-900/20 shadow-lg"
+>
+    <div
+        bind:this={titleBar}
+        class="title-bar cursor-move bg-gradient-to-b from-gray-100 to-gray-300 text-center text-sm font-medium text-gray-600"
+    >
+        <span>Create card</span>
+    </div>
+    <div class="overflow-auto p-4">
+        <slot />
+    </div>
+</div>
+
+<style>
+    .window {
+        position: fixed;
+        overflow: hidden;
+        resize: both;
+        user-select: none;
+    }
+</style>
