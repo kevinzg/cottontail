25ba918675c4606e3513858ba828edb8f48fdab6
Kevin Zúñiga <kevin.zun@gmail.com>
2022-04-04 21:25:11 -0500

    feat: add editor component using quill

---
+++ src/lib/Editor.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import Quill from 'quill';
+    import { onMount } from 'svelte';
+
+    let div: HTMLDivElement;
+    let quill: Quill;
+
+    onMount(() => {
+        quill = new Quill(div, {
+            theme: 'snow',
+        });
+    });
+
+    export const getContent = () => ({
+        text: quill.getText(),
+        html: quill.root.innerHTML,
+        delta: quill.getContents(),
+    });
+</script>
+
+<div class="bg-gray-50">
+    <div bind:this={div} class="h-32 bg-white" />
+</div>
+
+<style>
+    @import 'quill/dist/quill.snow.css';
+</style>
