337fc146b6f8f288de395cf3e2c251dc18bfe8f4
Kevin Zúñiga <kevin.zun@gmail.com>
2022-04-15 10:27:41 -0500

    feat: use tiptap editor instead of quill

--- src/lib/Editor.svelte
+++ src/lib/Editor.svelte
@@ -1,27 +1,48 @@
 <script lang="ts">
-    import Quill from 'quill';
-    import { onMount } from 'svelte';
+    import { onMount, onDestroy } from 'svelte';
+    import { Editor } from '@tiptap/core';
+    import StarterKit from '@tiptap/starter-kit';
+    import Image from '@tiptap/extension-image';
 
-    let div: HTMLDivElement;
-    let quill: Quill;
+    let element: HTMLDivElement;
+    let editor: Editor;
 
     onMount(() => {
-        quill = new Quill(div, {
-            theme: 'snow',
+        editor = new Editor({
+            element: element,
+            extensions: [StarterKit, Image],
+            editorProps: {
+                attributes: {
+                    class: 'prose prose-sm m-2 focus:outline-none',
+                },
+            },
+            content: '',
+            onTransaction: () => {
+                // force re-render so `editor.isActive` works as expected
+                // editor = editor;
+            },
         });
     });
 
-    export const getContent = () => ({
-        text: quill.getText(),
-        html: quill.root.innerHTML,
-        delta: quill.getContents(),
+    onDestroy(() => {
+        if (editor) {
+            editor.destroy();
+        }
     });
+
+    export function getContent() {
+        return {
+            text: editor.getText(),
+            html: editor.getHTML(),
+            json: editor.getJSON(),
+        };
+    }
 </script>
 
-<div class="bg-gray-50">
-    <div bind:this={div} class="h-32 bg-white" />
-</div>
+<div class="border border-gray-300 bg-white" bind:this={element} />
 
 <style>
-    @import 'quill/dist/quill.snow.css';
+    :global(.ProseMirror) {
+        min-height: 8rem;
+    }
 </style>
