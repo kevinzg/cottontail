126566319ff550e68073bbafa30ea3f75e689ad3
Kevin Zúñiga <kevin.zun@gmail.com>
2022-04-15 12:23:27 -0500

    feat: list cards in dashboard view

--- src/Dashboard.svelte
+++ src/Dashboard.svelte
@@ -1,4 +1,23 @@
 <script lang="ts">
+    import { liveQuery } from 'dexie';
+    import type { Readable } from 'svelte/store';
+    import { Database } from './lib/Database';
+    import type { ICard } from './lib/types';
+
+    const db = new Database();
+
+    // @ts-ignore
+    let cards = liveQuery<ICard[]>(() => db.cards.toArray()) as Readable<
+        ICard[]
+    >;
 </script>
 
-<h1>Cottontail Dashboard</h1>
+<h1 class="text-lg font-medium">Cottontail Dashboard</h1>
+<hr />
+<ul>
+    {#if $cards}
+        {#each $cards as card (card.id)}
+            <li>{card.title}</li>
+        {/each}
+    {/if}
+</ul>
