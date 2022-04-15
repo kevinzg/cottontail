8ec29a1ca34bb3f76183db1e65fcc4538fc3a453
Kevin Zúñiga <kevin.zun@gmail.com>
2022-04-15 11:13:06 -0500

    feat: add missing fields to form

--- src/lib/Form.svelte
+++ src/lib/Form.svelte
@@ -8,16 +8,19 @@
 
     let editor: Editor;
     let title = '';
+    let category = 'default';
+    let sourceUrl = window.location.toString();
+    let sourceName = window.document.title;
 
     const submit = () => {
         const content = editor.getContent();
         const card: ICardData = {
             title,
             content,
-            category: 'default',
+            category: category || 'default',
             source: {
-                url: window.location.toString(),
-                name: window.document.title,
+                url: sourceUrl,
+                name: sourceName,
             },
         };
         dispatch('save', card);
@@ -25,30 +28,64 @@
 </script>
 
 <form on:submit|preventDefault={submit} class="space-y-1">
-    <fieldset>
-        <label class="font-medium text-gray-700" for="back">Content</label>
-        <Editor bind:this={editor} />
-    </fieldset>
-
     <fieldset class="flex flex-col">
-        <label class="font-medium text-gray-700" for="name">Title</label>
+        <label class="text-sm font-medium text-gray-700" for="name">Title</label
+        >
         <input
             id="name"
             name="name"
-            placeholder="(optional)"
-            class="border border-gray-300 py-1 px-1 text-sm text-gray-800 placeholder:text-sm placeholder:italic placeholder:text-gray-400"
+            required
+            class="ml-2 border border-gray-300 py-1 px-1 text-sm text-gray-800 placeholder:text-sm placeholder:italic placeholder:text-gray-400"
             bind:value={title}
         />
     </fieldset>
 
-    <div class="flex justify-around py-2">
-        <button
-            on:click|preventDefault={() => dispatch('close')}
-            class="rounded bg-red-300 p-1 px-4 text-gray-800"
+    <fieldset>
+        <label class="text-sm font-medium text-gray-700" for="content"
+            >Content</label
         >
-            Close
-        </button>
-        <button class="rounded bg-blue-300 p-1 px-4 text-gray-800">
+        <div class="ml-2">
+            <Editor id="content" bind:this={editor} />
+        </div>
+    </fieldset>
+
+    <fieldset class="flex flex-col">
+        <label class="text-sm font-medium text-gray-700" for="category"
+            >Category</label
+        >
+        <input
+            id="category"
+            name="category"
+            placeholder="default"
+            class="ml-2 border border-gray-300 py-1 px-1 text-sm text-gray-800 placeholder:text-sm placeholder:italic placeholder:text-gray-400"
+            bind:value={category}
+        />
+    </fieldset>
+
+    <fieldset class="flex flex-col">
+        <label class="text-sm font-medium text-gray-700" for="sourceName"
+            >Source</label
+        >
+        <div class="ml-2 flex flex-row space-x-1.5">
+            <input
+                id="sourceName"
+                name="sourceName"
+                placeholder="Name"
+                class="grow border border-gray-300 py-1 px-1 text-sm text-gray-800 placeholder:text-sm placeholder:italic placeholder:text-gray-400"
+                bind:value={sourceName}
+            />
+            <input
+                id="sourceUrl"
+                name="sourceUrl"
+                placeholder="URL"
+                class="border border-gray-300 py-1 px-1 text-sm text-gray-800 placeholder:text-sm placeholder:italic placeholder:text-gray-400"
+                bind:value={sourceUrl}
+            />
+        </div>
+    </fieldset>
+
+    <div class="flex justify-around py-2">
+        <button class="rounded bg-blue-300 p-1 px-4 text-gray-900">
             Submit
         </button>
     </div>
