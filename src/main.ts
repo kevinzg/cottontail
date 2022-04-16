35aaee71029dc7f7a6d24b1c40d077147a9a4633
Kevin Zúñiga <kevin.zun@gmail.com>
2022-04-15 20:35:11 -0500

    fix: inject css on dev mode

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import App from './App.svelte';
+import './tailwind.css';
+import { style as TiptapCSS } from '@tiptap/core/src/style';
 import './main.css';
+
+const styleElement = window.document.createElement('style');
+styleElement.textContent = TiptapCSS;
+window.document.head.appendChild(styleElement);
 
 const app = new App({
     target: document.getElementById('app')!,
