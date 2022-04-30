8ecec95738060b172176871ebcd97a30b2cc569c
Kevin Zúñiga <kevin.zun@gmail.com>
2022-04-29 21:34:05 -0500

    fix: use ems for consistent size

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,39 @@
+const theme = require('tailwindcss/defaultTheme');
+
+// From https://github.com/tailwindlabs/tailwindcss/issues/800#issuecomment-974620969
+const deepMap = (val, fn) => {
+    const it = (val) => {
+        if (Array.isArray(val)) {
+            return val.map(it);
+        }
+        if (typeof val === 'object') {
+            const res = {};
+            for (const key of Object.keys(val)) {
+                res[key] = it(val[key]);
+            }
+            return res;
+        }
+        return fn(val);
+    };
+    return it(val);
+};
+
+const emify = (val) => {
+    if (typeof val === 'string') {
+        val = val.replace(/((\d*\.)?\d+)rem\b/g, (_, num) => `${num}em`);
+    }
+    return val;
+};
+
 module.exports = {
     content: ['./src/**/*.svelte'],
     theme: {
-        extend: {},
+        spacing: {
+            ...deepMap(theme.spacing, emify),
+        },
+        fontSize: {
+            ...deepMap(theme.fontSize, emify),
+        },
     },
     plugins: [
         require('@tailwindcss/forms'),
