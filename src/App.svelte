4e97b1ee048528dbf0b535fa334c3b48c1a05ff2
Kevin Zúñiga <kevin.zun@gmail.com>
2023-03-11 11:02:46 -0500

    feat: add dashboard to development app

--- src/App.svelte
+++ src/App.svelte
@@ -1,37 +1,58 @@
 <script lang="ts">
     import Cottontail from './Cottontail.svelte';
+    import Dashboard from './Dashboard.svelte';
+
+    let tab: 'sample' | 'dashboard' = 'dashboard';
 </script>
 
-<main class="mx-auto my-8 w-96 space-y-4 text-gray-800">
-    <h1 class="text-center text-3xl font-bold">Sample text</h1>
-    <img
-        alt="Black dunes"
-        src="https://source.unsplash.com/8xznAGy4HcY/800x400"
-    />
-    <p>
-        Aut omnis harum dolore itaque unde. Perspiciatis quis eum eveniet ab.
-        Quia voluptatem tempore cumque libero. Et quas assumenda porro
-        explicabo. Assumenda ab provident ullam voluptatem.
-    </p>
-    <p>
-        Aliquid minus sunt velit officia est quod. Et rerum asperiores dolores
-        architecto maiores iure sed praesentium. Laudantium ipsam veritatis et
-        sapiente molestiae enim odio temporibus. Pariatur aut laborum et nisi
-        debitis.
-    </p>
-    <p>
-        Laboriosam dicta blanditiis veritatis dolorem et. Architecto in omnis
-        minus. In quae consequatur est voluptatibus doloremque porro sunt.
-        Voluptate quasi laborum voluptates cumque reprehenderit quos architecto.
-    </p>
-    <p>
-        Debitis eum earum et dolorum aspernatur sint exercitationem. Et eum
-        quasi tempore ea. Et sed modi rerum dicta molestias facere aliquam.
-    </p>
-    <p>
-        Neque et ex ut voluptatem. Quia quo nesciunt quia numquam. Vero
-        reiciendis aut nostrum rerum vel quidem adipisci.
-    </p>
-</main>
+<div class="mx-auto w-fit bg-gray-50 py-1">
+    <label>
+        <input type="radio" bind:group={tab} value="sample" />
+        Sample page
+    </label>
+    <label>
+        <input type="radio" bind:group={tab} value="dashboard" />
+        Dashboard
+    </label>
+</div>
 
-<Cottontail on:save={(ev) => console.log('save', ev.detail)} />
+<hr />
+
+{#if tab === 'sample'}
+    <main class="mx-auto my-8 w-96 space-y-4 text-gray-800">
+        <h1 class="text-center text-3xl font-bold">Sample text</h1>
+        <img
+            alt="Black dunes"
+            src="https://source.unsplash.com/8xznAGy4HcY/800x400"
+        />
+        <p>
+            Aut omnis harum dolore itaque unde. Perspiciatis quis eum eveniet
+            ab. Quia voluptatem tempore cumque libero. Et quas assumenda porro
+            explicabo. Assumenda ab provident ullam voluptatem.
+        </p>
+        <p>
+            Aliquid minus sunt velit officia est quod. Et rerum asperiores
+            dolores architecto maiores iure sed praesentium. Laudantium ipsam
+            veritatis et sapiente molestiae enim odio temporibus. Pariatur aut
+            laborum et nisi debitis.
+        </p>
+        <p>
+            Laboriosam dicta blanditiis veritatis dolorem et. Architecto in
+            omnis minus. In quae consequatur est voluptatibus doloremque porro
+            sunt. Voluptate quasi laborum voluptates cumque reprehenderit quos
+            architecto.
+        </p>
+        <p>
+            Debitis eum earum et dolorum aspernatur sint exercitationem. Et eum
+            quasi tempore ea. Et sed modi rerum dicta molestias facere aliquam.
+        </p>
+        <p>
+            Neque et ex ut voluptatem. Quia quo nesciunt quia numquam. Vero
+            reiciendis aut nostrum rerum vel quidem adipisci.
+        </p>
+    </main>
+
+    <Cottontail on:save={(ev) => console.log('save', ev.detail)} />
+{:else if tab === 'dashboard'}
+    <Dashboard />
+{/if}
