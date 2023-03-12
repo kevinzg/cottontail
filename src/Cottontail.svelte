9aff00924c4b0a481b4bd677ec5ac5888a925321
Kevin Zúñiga <kevin.zun@gmail.com>
2023-03-11 21:37:15 -0500

    feat: redesign card list screen

--- src/Cottontail.svelte
+++ src/Cottontail.svelte
@@ -2,7 +2,7 @@
     import { createEventDispatcher } from 'svelte';
 
     import Form from './lib/Form.svelte';
-    import type { ICardData } from './lib/types';
+    import type { IFlashcardData } from './lib/types';
     import Window from './lib/Window.svelte';
 
     const dispatch = createEventDispatcher();
@@ -10,7 +10,8 @@
     let hidden = false;
     export let path: 'form' | 'list' = 'form';
 
-    let localCards: ICardData[] = [];
+    // TODO: Get cards for this domain/URL
+    let localCards: IFlashcardData[] = [];
 
     export function open() {
         hidden = false;
@@ -20,11 +21,12 @@
         hidden = true;
     }
 
-    function save(ev: CustomEvent<ICardData>) {
+    function save(ev: CustomEvent<IFlashcardData>) {
+        const card = ev.detail;
         path = 'list';
-        localCards.push(ev.detail);
+        localCards.push(card);
         localCards = localCards;
-        dispatch('save', ev.detail);
+        dispatch('save', card);
     }
 </script>
 
@@ -38,24 +40,29 @@
                 on:save={save}
             />
         {:else if path === 'list'}
-            <ul>
-                {#each localCards as card}
-                    <li>{card.title}</li>
-                {/each}
-            </ul>
-            <button
-                class="rounded bg-blue-300"
-                on:click={() => (path = 'form')}
-            >
-                New card
-            </button>
+            <div class="flex h-72 flex-col space-y-1 text-gray-800">
+                <div class="mx-auto w-fit">
+                    <button
+                        class="mt-2 rounded border border-gray-400 bg-white py-1 px-2 text-sm font-semibold text-gray-800 shadow hover:bg-gray-100"
+                        on:click={() => (path = 'form')}
+                    >
+                        New Card
+                    </button>
+                </div>
+                <ul class="list-outside list-disc pl-4">
+                    {#each localCards as card}
+                        <!-- TODO: Click to edit card -->
+                        <li class="max-w-lg">{card.front}</li>
+                    {/each}
+                </ul>
+            </div>
         {/if}
     </Window>
 
     {#if hidden}
         <div class="fixed bottom-5 left-5 z-[9999]">
             <button
-                class="flex h-12 w-12 items-center justify-center rounded-full bg-blue-200"
+                class="flex h-12 w-12 items-center justify-center rounded-full border border-gray-400 bg-white shadow hover:bg-gray-100"
                 on:click|preventDefault={open}
             >
                 🐰
